d3907eafcdcebb660988e18948dd122bf55518db
Josh Cooter <thejoshcooter@gmail.com>
2019-06-18 07:15:58 -0500

    project setup

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,25 @@
+// import enhancer functions
 const enhancer = require('./enhancer.js');
-// test away!
+
+// Item Objects
+// Items have name, durability and enhancement.
+// The item's enhancement it's a number from 0 to 20.
+// The item's durability it's a number from 0 to 100.
+
+// enhancer tests
+describe('enhancer.js tests', () => {
+    // test repair function
+    describe('repair()', () => {
+        
+    });
+
+    // test succeed function
+    describe('succeed()', () => {
+
+    });
+
+    // test fail function
+    describe('fail()', () => {
+
+    });
+});
